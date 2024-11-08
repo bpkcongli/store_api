d5632b837bb444c219b40fc880fabf587f7fb75e
bpkcongli <andrian8367@gmail.com>
2024-11-09 02:46:04 +0700

    feat: migration for create users table

---
+++ app/Database/Migrations/2024-11-08-192647_CreateUsersTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'CHAR',
+                'constraint' => 36,
+                'null' => false,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'null' => false,
+                'unique' => true,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => false,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true, true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
